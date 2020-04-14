58c8e82207dafa521385b043ef36ad63a3c7e7dd
Michael D. Norman <michael.norman@denormans.com>
2020-04-14 10:03:37 -0500

    update to get cognito identity

--- src/plugin.js
+++ src/plugin.js
@@ -1,154 +1,220 @@
-const AWSCognito = require('amazon-cognito-identity-js')
-const jwtDecode = require('jwt-decode');
-const CryptoJS = require('crypto-js')
+const AWSCognito = require('amazon-cognito-identity-js');
+const AWS = require('aws-sdk');
 
-// Get JWT Token from Cognito
-const session = ({ Username, Password, UserPoolId, ClientId, TokenType }) => new Promise((resolve, reject) => {
-  new AWSCognito.CognitoUser({
-    Username,
-    Pool: new AWSCognito.CognitoUserPool({
-      UserPoolId,
-      ClientId,
-    })
-  }).authenticateUser(new AWSCognito.AuthenticationDetails({
-    Username,
-    Password
-  }),
-  {
-    onSuccess: result => {
-      TokenType === 'id' ? resolve(result.idToken.jwtToken): resolve(result.accessToken.jwtToken);
-    },
-    onFailure: error => {
-      console.log(error);
-      reject(error);
-    }
+// Login with Cognito User Pool
+const authenticate = ({ Username, Password, UserPoolId, ClientId }) =>
+  new Promise((resolve, reject) => {
+    const userPool = new AWSCognito.CognitoUserPool({ UserPoolId, ClientId });
+    const cognitoUser = new AWSCognito.CognitoUser({
+      Username,
+      Pool: userPool,
+    });
+    const authenticationDetails = new AWSCognito.AuthenticationDetails({
+      Username,
+      Password,
+    });
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: (result) => {
+        resolve({
+          idToken: result.getIdToken().getJwtToken(),
+          accessToken: result.getAccessToken().getJwtToken(),
+        });
+      },
+      onFailure: (error) => {
+        console.error(error);
+        reject(error);
+      },
+      newPasswordRequired: function () {
+        console.error('Given user needs to set a new password');
+        reject('Given user needs to set a new password');
+      },
+      mfaRequired: function () {
+        console.error('MFA is not currently supported');
+        reject('MFA is not currently supported');
+      },
+      customChallenge: function () {
+        console.error('Custom challenge is not currently supported');
+        reject('Custom challenge is not currently supported');
+      },
+    });
   });
-});
 
-// Validate if the token has expired
-const validToken = token => {
-  const now = Date.now().valueOf() / 1000
-  const data = jwtDecode(token)
-  if (typeof data.exp !== 'undefined' && data.exp < now) {
-    return false
+// Get temporary credentials
+async function getCredentials(userTokens, { UserPoolId, IdentityPoolId, CognitoRegion }) {
+  console.log('Getting temporary credentials');
+
+  const logins = {};
+  const { idToken } = userTokens;
+
+  logins[`cognito-idp.${CognitoRegion}.amazonaws.com/${UserPoolId}`] = idToken;
+
+  AWS.config.update({ region: CognitoRegion });
+  const credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId,
+    Logins: logins,
+  });
+
+  await credentials.getPromise();
+
+  console.log('Got credentials:', credentials);
+
+  return credentials;
+}
+
+// Validate if the credentials have expired
+const validCredentials = (credentials) => {
+  if (credentials.error) {
+    return true;
   }
-  if (typeof data.nbf !== 'undefined' && data.nbf > now) {
-    return false
+
+  const now = Date.now().valueOf();
+  return now < credentials.expireTime;
+};
+
+const loadCredentials = async (context, key) => {
+  const credentialsJson = await context.store.getItem(key);
+  try {
+    const credentials = credentialsJson && JSON.parse(credentialsJson);
+
+    if (credentials && validCredentials(credentials)) {
+      if (credentials.error) {
+        // Display error
+        console.error('Error credentials:', credentials.error);
+        throw credentials.error;
+      }
+
+      return credentials;
+    }
+  } catch (e) {
+    console.error('Error loading credentials:', e);
+    context.store.removeItem(key);
   }
-  return true
-}
 
-// Encode our token
-const base64url = (source) => {
-  encodedSource = CryptoJS.enc.Base64.stringify(source);
-  encodedSource = encodedSource.replace(/=+$/, '');
-  encodedSource = encodedSource.replace(/\+/g, '-');
-  encodedSource = encodedSource.replace(/\//g, '_');
-  return encodedSource;
-}
+  return null;
+};
 
-// Create a fake token to keep in store, so we don't query for same wrong values
-const errorToken = error => {
-  const header = {
-    "alg": "HS256",
-    "typ": "JWT"
-  };
-  const stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
-  const encodedHeader = base64url(stringifiedHeader);
-  // If error we keep it for 1 min
-  const exp = (Date.now().valueOf() / 1000) + 60
-  const data = {
-    error,
-    exp
-  };
-  const stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data));
-  const encodedData = base64url(stringifiedData);
-  return encodedHeader + "." + encodedData;
-}
+const saveCredentials = (context, key, { accessKeyId, secretAccessKey, sessionToken, expireTime }) =>
+  context.store.setItem(
+    key,
+    JSON.stringify({ accessKeyId, secretAccessKey, sessionToken, expireTime: expireTime.valueOf() }),
+  );
 
 // Main run function
-const run = async (context, Username, Password, UserPoolId, ClientId, TokenType) => {
+const run = async (
+  context,
+  Username,
+  Password,
+  UserPoolId,
+  ClientId,
+  IdentityPoolId,
+  CognitoRegion,
+  CredentialType,
+) => {
   if (!Username) {
-    throw new Error('Username attribute is required')
+    throw new Error('Username attribute is required');
   }
   if (!Password) {
-    throw new Error('Password attribute is required')
+    throw new Error('Password attribute is required');
   }
   if (!UserPoolId) {
-    throw new Error('UserPoolId attribute is required')
+    throw new Error('UserPoolId attribute is required');
   }
   if (!ClientId) {
-    throw new Error('ClientId attribute is required')
+    throw new Error('ClientId attribute is required');
   }
-  if (!TokenType) {
-    TokenType = 'access'
+  if (!IdentityPoolId) {
+    throw new Error('IdentityPoolId attribute is required');
+  }
+  if (!CognitoRegion) {
+    throw new Error('CognitoRegion attribute is required');
+  }
+  if (!CredentialType) {
+    throw new Error('CredentialType attribute is required');
   }
 
-  const key = [ Username, Password, UserPoolId, ClientId, TokenType ].join('::')
-  const token = await context.store.getItem(key)
-  if (token && validToken(token)) {
-    if (jwtDecode(token).error){
-      // Display error
-      return jwtDecode(token).error
-    }
-    // JWT token is still valid, reuse it
-    return token
-  } else {
-    // Compute a new token
-    try {
-      const token = await session({ Username, Password, UserPoolId, ClientId, TokenType })
-      await context.store.setItem(key, token)
-      return token
-    }
-    catch(error){
-      // To keep thing simle we create a fake JWT token with error message
-      const token = errorToken(error.message)
-      await context.store.setItem(key, token)
-      return error.message
-    }
+  const key = [Username, Password, UserPoolId, ClientId, IdentityPoolId, CognitoRegion].join('::');
+  const credentials = await loadCredentials(context, key);
+  if (credentials) {
+    // JWT token is still valid, reuse the credentials
+    return credentials[CredentialType];
   }
-}
 
-module.exports.templateTags = [{
-  name: 'AwsCognitoToken',
-  displayName: 'AWS Cognito Token',
-  description: 'Plugin for Insomnia to provide Cognito JWT token from AWS',
-  args: [
-    {
-      displayName: 'Username',
-      type: 'string',
-      validate: arg => (arg ? '' : 'Required')
-    },
-    {
-      displayName: 'Password',
-      type: 'string',
-      validate: arg => (arg ? '' : 'Required')
-    },
-    {
-      displayName: 'UserPoolId',
-      type: 'string',
-      validate: arg => (arg ? '' : 'Required')
-    },
-    {
-      displayName: 'ClientId',
-      type: 'string',
-      validate: arg => (arg ? '' : 'Required')
-    },
-    {
-      displayName: "TokenType",
-      type: "enum",
-      defaultValue: "access",
-      options: [
-        {
-          displayName: "access",
-          value: "access"
-        },
-        {
-          displayName: "id",
-          value: "id"
-        }
-      ]
-    },
-  ],
-  run
-}];
+  // Compute a new token
+  try {
+    const token = await authenticate({
+      Username,
+      Password,
+      UserPoolId,
+      ClientId,
+      IdentityPoolId,
+    });
+    const newCredentials = await getCredentials(token, { UserPoolId, IdentityPoolId, CognitoRegion });
+    await saveCredentials(context, key, newCredentials);
+
+    return newCredentials[CredentialType];
+  } catch (error) {
+    console.error(error.message);
+    await saveCredentials(context, key, { error: error.message });
+    throw error.message;
+  }
+};
+
+module.exports.templateTags = [
+  {
+    name: 'AwsCognitoIdentity',
+    displayName: 'AWS Cognito Identity',
+    description: 'Plugin for Insomnia to provide Cognito Identity login from AWS',
+    args: [
+      {
+        displayName: 'Username',
+        type: 'string',
+        validate: (arg) => (arg ? '' : 'Required'),
+      },
+      {
+        displayName: 'Password',
+        type: 'string',
+        validate: (arg) => (arg ? '' : 'Required'),
+      },
+      {
+        displayName: 'UserPoolId',
+        type: 'string',
+        validate: (arg) => (arg ? '' : 'Required'),
+      },
+      {
+        displayName: 'ClientId',
+        type: 'string',
+        validate: (arg) => (arg ? '' : 'Required'),
+      },
+      {
+        displayName: 'IdentityPoolId',
+        type: 'string',
+        validate: (arg) => (arg ? '' : 'Required'),
+      },
+      {
+        displayName: 'CognitoRegion',
+        type: 'string',
+        validate: (arg) => (arg ? '' : 'Required'),
+      },
+      {
+        displayName: 'CredentialType',
+        type: 'enum',
+        defaultValue: 'accessKeyId',
+        options: [
+          {
+            displayName: 'accessKeyId',
+            value: 'accessKeyId',
+          },
+          {
+            displayName: 'secretAccessKey',
+            value: 'secretAccessKey',
+          },
+          {
+            displayName: 'sessionToken',
+            value: 'sessionToken',
+          },
+        ],
+      },
+    ],
+    run,
+  },
+];
